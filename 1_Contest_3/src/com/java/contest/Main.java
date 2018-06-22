bcd2d8507931465f8834a54ad85da2a6b1e7a87d
SeonghwanKim13109 <reasonby13@naver.com>
2018-06-22 18:48:03 +0900

    hme math contest 3

---
+++ 1_Contest_3/src/com/java/contest/Main.java
@@ -0,0 +1,7 @@
+package com.java.contest;
+
+public class Main {
+	public static void main(String[] args) {
+		Sum sum = new Sum();
+	}
+}
---
+++ 1_Contest_3/src/com/java/contest/Sum.java
@@ -0,0 +1,40 @@
+package com.java.contest;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Sum {
+	private ArrayList<Integer> arrList = new ArrayList();
+	private int sum = 0;
+	
+	public Sum() {
+		checkCondition();
+		makeSum();
+		answer();
+	}
+	
+	public void checkCondition() {
+		int threeNum;
+		int twoNum;
+		
+		for(int i =1 ; i<10; i++) {
+			for(int j =1 ; j<10 ; j++) {
+				twoNum = i*10 + j;
+				threeNum = i*100+j;
+				if(threeNum % twoNum == 0)
+					arrList.add(twoNum);
+			}
+		}
+	}
+	
+	public void makeSum() {
+		Iterator<Integer> iterator = arrList.iterator();
+		while(iterator.hasNext()) {
+			sum +=  iterator.next();
+		}
+	}
+	
+	public void answer() {
+		System.out.println(sum);
+	}
+}
